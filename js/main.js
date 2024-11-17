c553851f734517be24c1ebd4dea1984cd9db8e3b
FielLouis <fiellouisomasas@gmail.com>
2024-11-17 19:23:11 +0800

    added comments or certain html, css, and js files along with adjustments for other requirements not met

--- js/main.js
+++ js/main.js
@@ -1,58 +1,77 @@
+// getting all elements with the 'product' class
 const products = document.querySelectorAll('.product');
 
 products.forEach((product) => {
+    // selecting all the element within the products with 'product-container' class 
     const productContainer = product.querySelector('.product-container');
+    
+    // selecting all the first img within the productContainer
     const img = productContainer.querySelector('img');  
 
+    // once mouse hovers or enters the element change pic
     productContainer.addEventListener('mouseenter', function() {
         img.src = 'img/product_background_02.png'; 
     });
 
+    // once mouse exits or not hovering over the element anymore revert back to original pic
     productContainer.addEventListener('mouseleave', function() {
         img.src = 'img/product_background_01.png';
     });
 });
 
+// getting all elements with 'way-of-contact' class
 const contacts = document.querySelectorAll('.way-of-contact');
 
 contacts.forEach((contact) => {
     let imgWrapper;
     let img;
 
+    // if the subelement of the contacts has the 'mail' class
     if(contact.querySelector('.mail')) {
+        // selecting the 'mail' class then its respective img element
         imgWrapper = contact.querySelector('.mail');
         img = imgWrapper.querySelector('img');
 
+        // changing the photo once cursor hovers over the imgWrapper
         imgWrapper.addEventListener('mouseenter', function() {
             img.src = 'img/icon_mail_02.png';
         });
 
+        // reverting the photo once the cursor leaves
         imgWrapper.addEventListener('mouseleave', function() {
             img.src = 'img/icon_mail_01.png';
         });
     }
 
+    // if the subelement of the contacts has the 'call' class
     if(contact.querySelector('.call')) {
+        // selecting the 'call' class then its respective img element
         imgWrapper = contact.querySelector('.call');
         img = imgWrapper.querySelector('img');
 
+        // changing the photo once cursor hovers over the imgWrapper
         imgWrapper.addEventListener('mouseenter', function() {
             img.src = 'img/icon_call_02.png';
         });
 
+        // reverting the photo once the cursor leaves
         imgWrapper.addEventListener('mouseleave', function() {
             img.src = 'img/icon_call_01.png';
         });
     }
 
+    // if the subelement of the contacts has the 'address' class
     if(contact.querySelector('.address')) {
+        // selecting the 'address' class then its respective img element
         imgWrapper = contact.querySelector('.address');
         img = imgWrapper.querySelector('img');
 
+        // changing the photo once cursor hovers over the imgWrapper
         imgWrapper.addEventListener('mouseenter', function() {
             img.src = 'img/icon_address_02.png';
         });
 
+        // reverting the photo once the cursor leaves
         imgWrapper.addEventListener('mouseleave', function() {
             img.src = 'img/icon_address_01.png';
         });
@@ -60,6 +79,12 @@ contacts.forEach((contact) => {
     
 });
 
+// array of objects containing information for each social media platform
+// hoevrImg = image to show on hover
+// originalImg = original image before hover
+// bgColor = background color to set on hover
+// hoverBorderColor = border color to set on hover
+// originalBorderColor = original border color
 const socialMediaData = [
     { id: 'facebook', hoverImg: 'img/icon_fb_02.png', originalImg: 'img/icon_fb_01.png', bgColor: '#5366B1', hoverBorderColor: '#5366B1', originalBorderColor: '#353535' },
     { id: 'twitter', hoverImg: 'img/icon_twitter_02.png', originalImg: 'img/icon_twitter_01.png', bgColor: '#44A6E4', hoverBorderColor: '#44A6E4', originalBorderColor: '#353535' },
@@ -70,16 +95,20 @@ const socialMediaData = [
     { id: 'vimeo', hoverImg: 'img/icon_vimeo_02.png', originalImg: 'img/icon_vimeo_01.png', bgColor: '#4CB4E3', hoverBorderColor: '#4CB4E3', originalBorderColor: '#353535' }
 ];
 
+// loop through each social media platform in the array
 socialMediaData.forEach(function(socialMedia) {
+    // getting the element by ID and selecting the img inside
     const element = document.getElementById(socialMedia.id);
     const img = element.querySelector('img');
     
+    // change styles when cursor hovers over the socialMedia
     element.addEventListener('mouseenter', function() {
         element.style.backgroundColor = socialMedia.bgColor;
         element.style.borderColor = socialMedia.hoverBorderColor;
         img.src = socialMedia.hoverImg;
     });
 
+    // revert to original style when cursor leaves
     element.addEventListener('mouseleave', function() {
         element.style.backgroundColor = '';  
         element.style.borderColor = socialMedia.originalBorderColor;
@@ -87,9 +116,13 @@ socialMediaData.forEach(function(socialMedia) {
     });
 });
 
+// add smooth scroll functionality to elements with class 'circle'
 document.querySelectorAll('.circle').forEach(circle => {
+    // add a click event listener to each circle element
     circle.addEventListener('click', function() {
+        // get the data-section attribute which corresponds to the section id to scroll to
         const sectionId = this.getAttribute('data-section');
+        // scroll to the section smoothly
         document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
     });
 });
--- js/scrolling.js
+++ js/scrolling.js
@@ -1,27 +1,44 @@
+// getting all the elements with the 'bg-snow' classes
 const bgSnows = document.querySelectorAll('.bg-snow');
+
+// getting all the elements with the 'trees' classes
 const bgTrees = document.querySelectorAll('.trees');
+
+// getting the element with the 'reindeer-left' class
 const reindeerLeft = document.querySelector('.reindeer-left');
+
+// getting the element with the 'reindeer-right' class
 const reindeerRight = document.querySelector('.reindeer-right');
 
+
+
 window.addEventListener('scroll', function() {
+    // getting the position based on the scroll activity
     const scrollPosition = window.scrollY;
 
+    // making all the backgrounds photos move left with separate speed
+    // with first bg photo moving slower than the second bg photo
     bgSnows.forEach((bgSnow) => {
         bgSnow.style.backgroundPosition = `${scrollPosition * 0.2}px 0, ${scrollPosition * 0.4}px 0`;
     });
 
+    // making all the tree backgrounds move right
     bgTrees.forEach((bgTree) => {
         bgTree.style.backgroundPosition = `-${scrollPosition * 0.6}px 0`;
     });
 
-    const moveX = scrollPosition * 0.4;
-    const moveY = scrollPosition * 0.3;
+    // constants for how the reindeers move depending on the scroll behavior
+    const moveX = scrollPosition * 0.4; // how fast it moves left or right
+    const moveY = scrollPosition * 0.3; // how fast it moves up or down
+
 
     if (reindeerLeft) {
+        // moving left and up if scrolling down while right and down scrolling up
         reindeerLeft.style.transform = `translate(-${moveX}px, -${moveY}px)`;
     }
 
     if (reindeerRight) {
+        // moving right and up if scrolling down while left and down scrolling up
         reindeerRight.style.transform = `translate(${moveX}px, -${moveY}px)`;
     }
     
